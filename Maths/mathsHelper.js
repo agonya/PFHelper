be6d833da5c1f83e3ad4b1de0ebb2ef4cf7eaf46
Tayfun Turgut <tcfturgut@gmail.com>
2019-11-16 13:39:14 +0300

    code revision

--- Maths/mathsHelper.js
+++ Maths/mathsHelper.js
@@ -3,6 +3,7 @@
  *
  * @class MathsHelper
  */
+
 class MathsHelper {
 
     /**
--- masterPhaser.js
+++ masterPhaser.js
@@ -1,8 +1,16 @@
-let colorsHelper, graphicsHelper;
-let sds;
 class MasterPhaser {
-
     constructor(config) {
+        this.importHelper = function (module) {
+            let list = Object.getOwnPropertyNames(module);
+            let logObj = {};
+            for (let i = 3; i < list.length - 1; i++) {
+                logObj[`property${i-2}`] = list[i];
+            }
+            for (let p in logObj) {
+                this[logObj[p]] = module[logObj[p]];
+            }
+        }
+
         if (config.scene)
             this.scene = config.scene;
 
@@ -20,24 +28,7 @@ class MasterPhaser {
 
         if (config.operations)
             this.importHelper(require('./Operations/operationsHelper').default);
-
-
-
-        // Utility Helper - ObjectRegister, dynamic tween, resizeManager, timer yok
-
     }
-
-    importHelper(module) {
-        let list = Object.getOwnPropertyNames(module);
-        let logObj = {};
-        for (let i = 3; i < list.length - 1; i++) {
-            logObj[`property${i-2}`] = list[i];
-        }
-        for (let p in logObj) {
-            this[logObj[p]] = module[logObj[p]];
-        }
-    }
-
 }
 
 export default MasterPhaser;
